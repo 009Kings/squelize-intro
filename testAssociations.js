bc274e2dc65a186faa30084cd3b82edf65330dcf
009kings <009kings@gmail.com>
2021-01-14 11:21:15 -0800

    Mike Schull has no pets

--- testAssociations.js
+++ testAssociations.js
@@ -8,6 +8,7 @@ addModel
 */
 
 const db = require('./models');
+const user = require('./models/user');
 
 // Make a pet
 // db.pet.findOrCreate({
@@ -35,13 +36,41 @@ const db = require('./models');
 // });
 
 // Load associated items `getPet`
-db.user.findByPk(1)
-.then(user => {
-    // load the pets for this user
-    user.getPets()
-    .then(pets => {
-        // do sth to this
-        console.log('🐳');
-        console.log(pets);
+// db.user.findByPk(1)
+// .then(user => {
+//     // load the pets for this user
+//     user.getPets()
+//     .then(pets => {
+//         // do sth to this
+//         console.log('🐳');
+//         console.log(pets);
+//     });
+// });
+
+// `setPet` and `addPet`
+// db.pet.findByPk(1)
+// .then(pet => {
+//     db.user.findByPk(5)
+//     .then(user => {
+//         // associate them
+//         user.addPet(pet)
+//         console.log('Adopt a pet to reduce crankiness')
+//     })
+// })
+
+// Eager Loading
+db.user.findAll({
+    include: [db.pet]
+}).then(users => {
+    // each user object (each item in the users array)
+    // has a .pets key with an array of pet objs
+    users.forEach(user => {
+        console.log(`${user.name}'s pets:`);
+        if (user.pets.length < 1) {
+            console.log(`--${user.name} has no pets 😢`);
+        }
+        user.pets.forEach(pet => {
+            console.log(`--- ${pet.name}`);
+        });
     });
 });
